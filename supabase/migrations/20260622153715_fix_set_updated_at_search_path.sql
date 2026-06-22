ceae5093a1afd3dbc42f3a6759a3282bf1bfe6c0
sushikikun <nakanerobo@gmail.com>
2026-06-23 00:56:28 +0900

    fix: set search_path on updated_at trigger function (#8)

---
+++ supabase/migrations/20260622153715_fix_set_updated_at_search_path.sql
@@ -0,0 +1,10 @@
+create or replace function public.set_updated_at()
+returns trigger
+language plpgsql
+set search_path = ''
+as $$
+begin
+  new.updated_at = pg_catalog.now();
+  return new;
+end;
+$$;
